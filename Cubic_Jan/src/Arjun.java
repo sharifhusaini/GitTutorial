7da153c273b9a1b253c0aa293bfb5cdae941dc60
Temesghen <habtetsion@gmail.com>
2017-04-28 12:22:38 -0500

    adding againg

--- Cubic_Jan/src/Arjun.java
+++ Cubic_Jan/src/Arjun.java
@@ -5,10 +5,7 @@ public class Arjun {
 	public static void main(String[] args) {
 		System.out.println("Sample code added by Arjun");
 		System.out.println("My name is Temesghen");
-		System.out.println("Change made by Idris Git tutorial");
-		System.out.println("Nima Pakhrin");
-
-
+		
 	}
 
 }
