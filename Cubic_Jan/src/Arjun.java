a7a829664be0f3cfc86709176a3d26fe43833e64
avinandandahal <avidahal02@gmail.com>
2017-04-28 11:27:34 -0500

    changes made by Avi

--- Cubic_Jan/src/Arjun.java
+++ Cubic_Jan/src/Arjun.java
@@ -1,11 +1,10 @@
 
 public class Arjun {
 
-	/**
-	 * @param args
-	 */
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Sample code added by Arjun");
+		System.out.println("Sample code added by Avi");
 
 	}
 
