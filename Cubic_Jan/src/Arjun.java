5514843c84c65798f88955da6af19320378f170c
idris patan <idris patan>
2017-04-28 11:35:45 -0500

    Idris first change

--- Cubic_Jan/src/Arjun.java
+++ Cubic_Jan/src/Arjun.java
@@ -4,6 +4,8 @@ public class Arjun {
 	
 	public static void main(String[] args) {
 		System.out.println("Sample code added by Arjun");
+		
+		System.out.println("Change made by Idris Git tutorial");
 
 	}
 
