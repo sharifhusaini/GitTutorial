8ee3e311088e1c7b98786dfc354944b5725425a3
avinandandahal <avidahal02@gmail.com>
2017-04-28 12:13:17 -0500

    avi

--- Cubic_Jan/src/Arjun.java
+++ Cubic_Jan/src/Arjun.java
@@ -4,9 +4,17 @@ public class Arjun {
 	
 	public static void main(String[] args) {
 		System.out.println("Sample code added by Arjun");
+<<<<<<< HEAD
 		System.out.println("Sample code added by Avi");
 		System.out.println("by Mahesh");
 
+=======
+
+		
+		System.out.println("Change made by Idris Git tutorial");
+
+		System.out.println("Nima Pakhrin");
+>>>>>>> master
 
 
 	}
