3aed1462156f55f18b33354d85cc7119dc8afb83
mahesh <maheshluitel33@gmail.com>
2017-04-28 12:21:16 -0500

    by mahesh

--- Cubic_Jan/src/Arjun.java
+++ Cubic_Jan/src/Arjun.java
@@ -5,7 +5,23 @@ public class Arjun {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+<<<<<<< HEAD
 System.out.println("maheshhh");
+=======
+		System.out.println("Sample code added by Arjun");
+
+
+		System.out.println("Sample code added by Narottam"
+		
+
+		
+		System.out.println("Change made by Idris Git tutorial");
+
+
+		System.out.println("Nima Pakhrin");
+
+
+>>>>>>> master
 	}
 
 }
